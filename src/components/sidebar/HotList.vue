bc2dc1c41e8b2ea34b324e342fdc39801886f02a
macaoyuan <macaoyuan@peiwo.cn>
2020-03-27 19:11:09 +0800

    feat: add tips

--- src/components/sidebar/HotList.vue
+++ src/components/sidebar/HotList.vue
@@ -1,60 +1,30 @@
 <template>
   <div>
     <dl class="fly-panel fly-list-one">
-        <dt class="fly-panel-title">本周热议</dt>
-        <dd>
-          <a href="jie/detail.html">基于 layui 的极简社区页面模版</a>
-          <span><i class="iconfont icon-pinglun1"></i> 16</span>
-        </dd>
-        <dd>
-          <a href="jie/detail.html">基于 layui 的极简社区页面模版</a>
-          <span><i class="iconfont icon-pinglun1"></i> 16</span>
-        </dd>
-        <dd>
-          <a href="jie/detail.html">基于 layui 的极简社区页面模版</a>
-          <span><i class="iconfont icon-pinglun1"></i> 16</span>
-        </dd>
-        <dd>
-          <a href="jie/detail.html">基于 layui 的极简社区页面模版</a>
-          <span><i class="iconfont icon-pinglun1"></i> 16</span>
-        </dd>
-        <dd>
-          <a href="jie/detail.html">基于 layui 的极简社区页面模版</a>
-          <span><i class="iconfont icon-pinglun1"></i> 16</span>
-        </dd>
-        <dd>
-          <a href="jie/detail.html">基于 layui 的极简社区页面模版</a>
-          <span><i class="iconfont icon-pinglun1"></i> 16</span>
-        </dd>
-        <dd>
-          <a href="jie/detail.html">基于 layui 的极简社区页面模版</a>
-          <span><i class="iconfont icon-pinglun1"></i> 16</span>
-        </dd>
-        <dd>
-          <a href="jie/detail.html">基于 layui 的极简社区页面模版</a>
-          <span><i class="iconfont icon-pinglun1"></i> 16</span>
-        </dd>
-        <dd>
-          <a href="jie/detail.html">基于 layui 的极简社区页面模版</a>
-          <span><i class="iconfont icon-pinglun1"></i> 16</span>
-        </dd>
-        <dd>
-          <a href="jie/detail.html">基于 layui 的极简社区页面模版</a>
-          <span><i class="iconfont icon-pinglun1"></i> 16</span>
-        </dd>
+      <dt class="fly-panel-title">本周热议</dt>
+      <dd v-for="(item, index) in lists" :key="'hotlist' + index">
+        <a href="jie/detail.html">{{ item.title }}</a>
+        <span><i class="iconfont icon-pinglun1"></i> {{ item.answer }}</span>
+      </dd>
 
-        <!-- 无数据时 -->
-        <!--
+      <!-- 无数据时 -->
+      <!--
         <div class="fly-none">没有相关数据</div>
         -->
-      </dl>
+    </dl>
   </div>
 </template>
 <script>
+import { getTop } from '@/api/content'
 export default {
-  name: 'hotlist'
+  name: 'hotlist',
+  mounted() {
+    getTop().then(res => {
+      if (res.code === 200) {
+        this.lists = res.data
+      }
+    })
+  }
 }
 </script>
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
